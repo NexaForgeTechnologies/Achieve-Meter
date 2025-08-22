8606b21690a4a11204529d6d4269c42d360da979
NexaForgeTechnologies <project@nexaforgetech.com>
2025-08-22 13:48:57 +0500

    Update TwoAudienceCont.jsx

--- src/containers/TwoAudienceCont.jsx
+++ src/containers/TwoAudienceCont.jsx
@@ -37,16 +37,15 @@ export default function TwoAudienceCont() {
                             <ul className="flex flex-col gap-y-1 text-[#C2C2C2]">
                                 <li className={`flex items-start gap-x-2 mr-1`}>
                                     <p className="text-xs md:text-base text-[#808080] relative -top-1">
-                                        Personal career coach - built-in acceleration with AI, structure, and
-                                        social validation
+                                        Organization-wide workforce development intelligence, without replacing your LMS
                                     </p>
                                 </li>
-                                <li className={`flex items-start gap-x-2 mr-1`}>
-                                    <p className="text-xs md:text-base text-[#808080] relative -top-1">
-                                        Organization-wide workforce development intelligence, without
-                                        replacing your LMS
-                                    </p>
-                                </li>
+                                //<li className={`flex items-start gap-x-2 mr-1`}>
+                                  //  <p className="text-xs md:text-base text-[#808080] relative -top-1">
+                                   //     Organization-wide workforce development intelligence, without
+                                   //     replacing your LMS
+                                  //  </p>
+                              //  </li>
                             </ul>
                         </div>
 
